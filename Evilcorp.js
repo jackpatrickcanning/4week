65228e296fae05fc04d41fc75dc264cfac9eadee
jackpatrickcanning <71390494+jackpatrickcanning@users.noreply.github.com>
2020-11-05 13:38:24 +0000

    Modal - Covid 19

--- Evilcorp.js
+++ Evilcorp.js
@@ -235,5 +235,49 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+const ModalWindow = {
+    init() {
+        document.body.addEventListener("click", e => {
+            if (e.target.classList.contains("modal__close")) {
+                this.closeModal(e.target);
+            }
+        });
+      
+      this.openModal();
+    },
+
+    getHtmlTemplate(modalOptions) {
+        return `
+            <div class="modal__overlay">
+                <div class="modal__window">
+                    <div class="modal__titlebar">
+                        <span class="modal__title">${modalOptions.title}</span>
+                        <button class="modal__close material-icons">close</button>
+                    </div>
+                    <div class="modal__content">
+                        ${modalOptions.content}
+                    </div>
+                </div>
+            </div>
+        `;
+    },
+
+    openModal(modalOptions = {}) {
+        modalOptions = Object.assign({
+            title: 'Evil Corp - Covid 19',
+            content: "Our plan worked, Covid 19 is now a thing, so just a heads up to all henchman and employees a like, you're evil I like that. Remember that all HR Work is to carry on as normal so pleaase update your details, or you wont get paid for your evilness!!!"
+        }, modalOptions);
+
+        const modalTemplate = this.getHtmlTemplate(modalOptions);
+        document.body.insertAdjacentHTML("afterbegin", modalTemplate);
+    },
+
+    closeModal(closeButton) {
+        const modalOverlay = closeButton.parentElement.parentElement.parentElement;
+        document.body.removeChild(modalOverlay);
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => ModalWindow.init());
 
 displayData();
