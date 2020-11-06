7986815a8525c1238bf4beab507635681c68eb7e
jackpatrickcanning <71390494+jackpatrickcanning@users.noreply.github.com>
2020-11-06 12:04:43 +0000

    errors

---
+++ ERecords.js
@@ -0,0 +1,17 @@
+let records = [  
+    {"ninumber":"ZS502747A", "fullname": "Dr Draken", "phone": "07659-831024", "address": "123 Elliot Hill", "department":"IT"},  
+    {"ninumber":"XS130502B", "fullname": "Shego", "phone": "07666-616680", "address": "321 Haha Road", "department":"Assasin"},  
+    {"ninumber":"MY034526D", "fullname": "Monkey Fist", "phone": "07440-003065", "address": "64 Zoo Lane", "department":"HR"},  
+    {"ninumber":"AK311470", "fullname": "Señor Senior Senior ", "phone": "07342-472921", "address": "324 Langton Ridgeway", "department":"HR"},  
+    {"ninumber":"LY682275B", "fullname": "Señor Senior Junior ", "phone": "07822-821023", "address": "234 Julian Market", "department":"IT"},  
+    {"ninumber":"BK227215B", "fullname": "Duff Killigan ", "phone": "07661-522545", "address": "12 Springfield Grange", "department":"Assasin"},  
+    {"ninumber":"XB363374C", "fullname": "Motor Ed ", "phone": "07563-758264", "address": "54 Blackbird Crescent", "department":"IT"},
+    {"ninumber":"MY501327A", "fullname": "Synthodrone", "phone": "07401-414740", "address": "2 St Margarets Drive", "department":"IT"},
+    {"ninumber":"TT405395B", "fullname": "Camille Leon ", "phone": "07870-297789", "address": "98 Earl Path", "department":"IT"},
+    {"ninumber":"AZ764036A", "fullname": "Warmonga ", "phone": "07872-642897", "address": "233 Lady Smith Avenue", "department":"IT"},
+    {"ninumber":"ES73841C", "fullname": "Gill ", "phone": "07750-872412", "address": "45 Fountains Broadway", "department":"Assasin"},
+    {"ninumber":"WX465470A", "fullname": "Adrena Lynn ", "phone": "07773-782275", "address": "65 Burlington Lodge", "department":"HR"},
+    {"ninumber":"AK625470D", "fullname": "Warhok", "phone": "07569-060117", "address": "97 Holderness Drive", "department":"HR"},
+    {"ninumber":"SW098272B", "fullname": "DNAmy ", "phone": "07394-529507", "address": "32 Banningham Court", "department":"Assasin"},
+    {"ninumber":"OB043941D", "fullname": " Professor Dementor ", "phone": "07958-301691", "address": "3 Third Mount", "department":"Assasin"}
+];
